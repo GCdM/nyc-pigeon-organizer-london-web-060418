61c44791c4947dcd69d1545c4a863dfca6816613
Gabriel Miranda <gabrielcm.br@gmail.com>
2018-06-04 21:38:11 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,11 +1,11 @@
 require 'pry'
 
 def nyc_pigeon_organizer(data)
-  pigeon_list = Hash.new(Hash.new([]))
+  pigeon_list = {}
   data.each do |attribute, att_info|
     att_info.each do |type, pigeons|
       pigeons.each do |pigeon|
-        pigeon_list[pigeon][attribute] += [type]
+        pigeon_list[pigeon] += {attribute
       end
     end
   end
